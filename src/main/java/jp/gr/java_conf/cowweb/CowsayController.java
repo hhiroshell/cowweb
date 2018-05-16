00e7d7cfbc3b977730dd220959065c350c53438f
hhiroshell <hhiroshell@gmail.com>
2018-05-16 19:29:34 +0900

    Preparation for demo.

--- src/main/java/jp/gr/java_conf/cowweb/CowsayController.java
+++ src/main/java/jp/gr/java_conf/cowweb/CowsayController.java
@@ -25,14 +25,15 @@ public class CowsayController {
     AccessCounter counter;
 
     static {
-        List<String> infelicities = Arrays.asList(new String[]{"head-in", "telebears", "sodomized"});
-        List<String> c = new ArrayList<>();
-        Arrays.stream(Cowsay.say(new String[]{"-l"}).split(br)).forEach(f -> {
-            if (!infelicities.contains(f)) {
-                c.add(f);
-            }
-        });
-        cowfiles = Collections.unmodifiableList(c);
+        cowfiles = Arrays.asList(new String[]{"default"});
+//        List<String> infelicities = Arrays.asList(new String[]{"head-in", "telebears", "sodomized"});
+//        List<String> c = new ArrayList<>();
+//        Arrays.stream(Cowsay.say(new String[]{"-l"}).split(br)).forEach(f -> {
+//            if (!infelicities.contains(f)) {
+//                c.add(f);
+//            }
+//        });
+//        cowfiles = Collections.unmodifiableList(c);
     }
 
     /**
